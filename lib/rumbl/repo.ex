60451c9fdbbd0ec02a096e4fa62f35dce48a7c58
Riza Fahmi <rizafahmi@gmail.com>
2015-12-29 02:41:34 +0700

    Initiate Ecto
    Chapter 4 section 4

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,24 +1,4 @@
 defmodule Rumbl.Repo do
-  # use Ecto.Repo, otp_app: :rumbl
-  @moduledoc """
-  In memory repository.
-  """
+  use Ecto.Repo, otp_app: :rumbl
 
-  def all(Rumbl.User) do
-    [
-      %Rumbl.User{id: "1", name: "Riza", username: "rizafahmi", password: "220281"}
-    ]
-  end
-
-  def all(_module), do: []
-
-  def get(module, id) do
-    Enum.find all(module), fn map -> map.id == id end
-  end
-
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end
-  end
 end
