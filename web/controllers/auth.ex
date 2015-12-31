c8c644b3dcf855331c611be624d775f89f630497
Riza Fahmi <rizafahmi@gmail.com>
2015-12-31 21:30:28 +0700

    Finish generates video module
    Chapter 6

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -1,5 +1,7 @@
 defmodule Rumbl.Auth do
   import Plug.Conn
+  import Phoenix.Controller
+  alias Rumbl.Router.Helpers
   import Comeonin.Bcrypt, only: [checkpw: 2]
 
   def init(opts) do
@@ -35,5 +37,16 @@ defmodule Rumbl.Auth do
 
   def logout(conn) do
     configure_session(conn, drop: true)
+  end
+
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must logged in to access this page")
+      |> redirect(to: Helpers.page_path(conn, :index))
+      |> halt()
+    end
   end
 end
