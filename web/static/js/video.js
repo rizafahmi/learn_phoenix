c3903037d52efa39968d799fcd918c8e5166da95
Riza Fahmi <rizafahmi@gmail.com>
2016-01-02 09:04:47 +0700

    Scheduled Annotation. WIP
    Chapter 9 section 7

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -23,22 +23,61 @@ let Video = {
       msgInput.value = ""
     })
 
+    // msgContainer.addEventListener("click", e => {
+    //   e.preventDefault()
+    //   let seconds = e.target.getAttribute("data-seek")
+    //   if(!seconds) { return }
+
+    //   Player.seekTo(seconds)
+    // })
+
+    vidChannel.on("ping", ({count}) => console.log("PING", count))
+
     vidChannel.on("new_annotation", (resp) => {
       this.renderAnnotation(msgContainer, resp)
     })
 
-    vidChannel.on("ping", ({count}) => console.log("PING", count))
-
     vidChannel.join()
-      .receive("ok", resp => console.log("joined the video channel", resp))
+      .receive("ok", resp => {
+        this.scheduleMessages(msgContainer, resp.annotations)
+      })
       .receive("error", reason => console.log('join failed', reason))
 
   },
-  renderAnnotation(msgContainer, {user, body, at}) {
+  renderAnnotation(msgContainer, {user, body, at}){
     let template = document.createElement("div")
-    template.innerHTML = `<b>${user.username}</b>: ${body}`
+    template.innerHTML = `
+      <a href="#" data-seek="${at}">
+      [${this.formatTime(at)}] <b>${user.username}</b>: ${body}
+    </a>
+      `
     msgContainer.appendChild(template)
     msgContainer.scrollTop = msgContainer.scrollHeight
+  },
+  formatTime(at) {
+    let date = new Date(null)
+    date.setSeconds(at / 1000)
+    return date.toISOString().substr(14, 5)
+  },
+  scheduleMessages(msgContainer, annotations) {
+    setTimeout(() => {
+      debugger
+      let ctime = 0
+      if (Player)
+        Player.getCurrentTime()
+      let remaining = this.renderAtTime(annotations, ctime, msgContainer)
+      this.scheduleMessages(msgContainer, remaining)
+    })
+  },
+  renderAtTime(annotations, seconds, msgContainer) {
+    return annotations.filter( ann => {
+      if (ann.at > seconds ) {
+        return true
+      } else {
+        this.renderAnnotation(msgContainer, ann)
+        return false
+      }
+    })
   }
 }
 
