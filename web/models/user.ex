af07c15ffa096e1654891d10a514692cef84dea9
Riza Fahmi <rizafahmi@gmail.com>
2016-02-11 20:16:01 +0700

    Ecto chapter 4. WIP

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,3 +1,13 @@
 defmodule Rumbl.User do
+  use Rumbl.Web, :model
 
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamp
+
+  end
 end
